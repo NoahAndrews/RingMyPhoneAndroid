10dc417125da7a003ba38ef7d4ecc1477989f0e8
NoahAndrews <noah@noahandrews.me>
2022-02-13 15:50:21 -0600

    Failed vibration experiment

--- RingMyPhone/src/main/java/com/darkrockstudios/apps/ringmyphone/RingerService.java
+++ RingMyPhone/src/main/java/com/darkrockstudios/apps/ringmyphone/RingerService.java
@@ -1,6 +1,7 @@
 package com.darkrockstudios.apps.ringmyphone;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -20,12 +21,15 @@ import android.os.PowerManager;
 import androidx.core.app.NotificationCompat;
 import androidx.preference.PreferenceManager;
 
+import android.os.Vibrator;
 import android.util.Log;
 
 import com.getpebble.android.kit.Constants;
 import com.getpebble.android.kit.util.PebbleDictionary;
 
 import org.json.JSONException;
+
+import java.util.Locale;
 
 /**
  * Created by Adam on 10/14/13.
@@ -34,6 +38,8 @@ public class RingerService extends Service {
     private static final String TAG = RingerService.class.getSimpleName();
     public static final String ACTION_STOP_RINGING = RingerService.class.getName() + ".STOP_RINGING";
     private static final String RINGS_NOTIFICATION_CHANNEL = "ringsNotificationChannel";
+    private static final String RINGS_WITH_VIBRATION_NOTIFICATION_CHANNEL = "ringsWithVibrationNotificationChannel";
+    private static final long[] VIBRATION_PATTERN = new long[] { 1000, 1000 };
 
     private static final int CMD_KEY = 0x1;
 
@@ -42,17 +48,40 @@ public class RingerService extends Service {
 
     public static void createNotificationChannels(Context context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = context.getString(R.string.notification_channel_rings_name);
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel channel = new NotificationChannel(RINGS_NOTIFICATION_CHANNEL, name, importance);
-            channel.setSound(null, null);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
+            createNotificationChannel(context, RINGS_NOTIFICATION_CHANNEL);
+            createNotificationChannel(context, RINGS_WITH_VIBRATION_NOTIFICATION_CHANNEL);
         }
     }
 
+    @TargetApi(Build.VERSION_CODES.O)
+    private static void createNotificationChannel(Context context, String channelId) {
+        CharSequence name;
+        boolean vibrate = false;
+
+        switch (channelId) {
+            case RINGS_NOTIFICATION_CHANNEL:
+                name = context.getString(R.string.notification_channel_rings_name);
+                break;
+            case RINGS_WITH_VIBRATION_NOTIFICATION_CHANNEL:
+                name = context.getString(R.string.notification_channel_rings_with_vibration_name);
+                vibrate = true;
+                break;
+            default:
+                return;
+        }
+
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        NotificationChannel channel = new NotificationChannel(channelId, name, importance);
+        channel.setSound(null, null);
+        channel.enableVibration(vibrate);
+
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this
+        NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    private AudioManager audioManager;
     private PowerManager.WakeLock wakeLock;
     private Ringtone ringtone;
     private boolean currentlyOverridingVolume = false;
@@ -61,6 +90,10 @@ public class RingerService extends Service {
 
     public IBinder onBind(final Intent intent) {
         return null;
+    }
+
+    @Override public void onCreate() {
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
     }
 
     @Override
@@ -107,13 +140,19 @@ public class RingerService extends Service {
     }
 
     @SuppressLint("LaunchActivityFromNotification")
-    private void postRingingNotification() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, RINGS_NOTIFICATION_CHANNEL);
+    private void postRingingNotification(boolean vibrate) {
+        String channelId = vibrate ? RINGS_WITH_VIBRATION_NOTIFICATION_CHANNEL : RINGS_NOTIFICATION_CHANNEL;
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId);
         builder.setTicker(getString(R.string.notification_ringing_ticker));
         builder.setContentTitle(getString(R.string.notification_ringing_ticker));
         builder.setContentText(getString(R.string.notification_ringing_text));
         builder.setSmallIcon(R.drawable.ic_action_volume_up);
         builder.setSound(null);
+
+        if (vibrate) {
+            builder.setVibrate(VIBRATION_PATTERN);
+        }
 
         builder.setContentIntent(createStopRingingIntent());
         builder.setOngoing(true);
@@ -142,11 +181,10 @@ public class RingerService extends Service {
     private void setMaxVolume(final Context context) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        // TODO(Noah): If we cannot ring the phone because of DND mode, notify the watch app
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             int interruptionFilter = notificationManager.getCurrentInterruptionFilter();
             Log.d(TAG, "Current interruption filter: " + interruptionFilter);
-            if (interruptionFilter != NotificationManager.INTERRUPTION_FILTER_NONE) {
+            if (interruptionFilter != NotificationManager.INTERRUPTION_FILTER_ALL) {
                 Log.i(TAG, "Detected Do Not Disturb mode");
                 if (notificationManager.isNotificationPolicyAccessGranted()) {
                     Log.i(TAG, "We have permission to override Do Not Disturb mode");
@@ -158,30 +196,34 @@ public class RingerService extends Service {
             }
         }
 
-        AudioManager am =
-                (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 
-        savedVolume = am.getStreamVolume(AudioManager.STREAM_RING);
-        Log.i(TAG, "Remembering current volume: " + savedVolume);
-        savedRingerMode = am.getRingerMode();
+        // If the current ringer mode is RINGER_MODE_VIBRATE, then the ring volume will be
+        // remembered as 0, which is not correct. To get the user's real ringer volume, we set
+        // the ringer mode to RINGER_MODE_NORMAL first, and only then look up the ring volume.
+        // Tested on Android 12.
+
+        savedRingerMode = audioManager.getRingerMode();
         Log.i(TAG, "Remembering current ringer mode: " + savedRingerMode);
 
-        am.setStreamVolume(
+        audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+
+        savedVolume = audioManager.getStreamVolume(AudioManager.STREAM_RING);
+        Log.i(TAG, "Remembering current volume: " + savedVolume);
+
+        audioManager.setStreamVolume(
                 AudioManager.STREAM_RING,
-                am.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
+                audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
                 0);
         currentlyOverridingVolume = true;
     }
 
     private void restorePreviousVolume(final Context context) {
         if (currentlyOverridingVolume) {
-            AudioManager am =
-                    (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-
             Log.i(TAG, "Restoring volume to " + savedVolume);
-            am.setStreamVolume(AudioManager.STREAM_RING, savedVolume, 0);
+            audioManager.setStreamVolume(AudioManager.STREAM_RING, savedVolume, 0);
+
             Log.i(TAG, "Restoring ringer mode to " + savedRingerMode);
-            am.setRingerMode(savedRingerMode);
+            audioManager.setRingerMode(savedRingerMode);
             currentlyOverridingVolume = false;
         } else {
             Log.w(TAG, "Was not currently overriding the volume and ringer mode");
@@ -207,25 +249,62 @@ public class RingerService extends Service {
     private void ringPhone(final Context context, final boolean silentMode) {
         getWakeLock(context);
 
-        postRingingNotification();
+        int ringerMode = audioManager.getRingerMode();
+        double relativeVolume = (double) audioManager.getStreamVolume(AudioManager.STREAM_RING)
+                / audioManager.getStreamMaxVolume(AudioManager.STREAM_RING);
+        Log.d(TAG, String.format(Locale.ENGLISH, "ringerMode=%d relativeVolume=%f", ringerMode, relativeVolume));
+        boolean vibrate = false;
 
-        if (ringtone == null) {
-            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-            ringtone = RingtoneManager.getRingtone(context, notification);
+        if (!silentMode) {
+            // When testing on Android 12, I discovered that in Do Not Disturb mode, the ringer mode
+            // is always reported as RINGER_MODE_SILENT, even when it's actually set to VIBRATE or
+            // NORMAL and high-priority notifications are allowed through. As a result, we're only
+            // checking the ringer volume, not the ringer mode. The ringer volume gets temporarily
+            // set to 0 in the SILENT and VIBRATE ringer modes.
+
+            // We don't need to care about the case where we're not in Do Not Disturb mode, because
+            // if we're not in Do Not Disturb mode, then we were previously able to set the ringer
+            // mode and volume to how we want them (playing sound at max volume).
+
+            // TODO(Noah): Determine if vibration will succeed
+            if (relativeVolume < 0.01) {
+                Log.w(TAG, "Notifications are muted. Enabling vibration.");
+                vibrate = true;
+                // TODO(Noah): Inform the watch app that the phone is vibrating but the ringtone is not playing
+            } else if (relativeVolume <= 0.5) {
+                Log.w(TAG, "Ringer volume is less than half of the maximum. Enabling vibration.");
+                vibrate = true;
+                // TODO(Noah): Inform the watch app that the phone volume is low and the phone is vibrating
+            }
         }
 
-        if (ringtone != null && !ringtone.isPlaying() && !silentMode) {
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                        .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
-                        .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                        .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
-                        .build();
-                ringtone.setAudioAttributes(audioAttributes);
-            } else {
-                ringtone.setStreamType(AudioManager.STREAM_RING);
+        // TODO(Noah): Keep vibration going
+        audioManager.setMode(AudioManager.MODE_RINGTONE);
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(new long[] { 0, 1000, 500 }, 0);
+        // TODO(Noah): Cancel vibration
+
+        postRingingNotification(vibrate);
+
+        if (!silentMode) {
+            if (ringtone == null) {
+                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+                ringtone = RingtoneManager.getRingtone(context, notification);
             }
-            ringtone.play();
+
+            if (ringtone != null && !ringtone.isPlaying()) {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                            .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
+                            .build();
+                    ringtone.setAudioAttributes(audioAttributes);
+                } else {
+                    ringtone.setStreamType(AudioManager.STREAM_RING);
+                }
+                ringtone.play();
+            }
         }
     }
 
